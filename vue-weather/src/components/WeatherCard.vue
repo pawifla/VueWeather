b7348b2213fd757121d5d49b27006ff38c395991
pawifla <willflanagan10@gmail.com>
2023-10-12 12:22:29 -0700

    add autocomplete

--- vue-weather/src/components/WeatherCard.vue
+++ vue-weather/src/components/WeatherCard.vue
@@ -4,27 +4,38 @@
         This is the weather.
     </p>
     <h3>Weather</h3>
-    <input type="text" v-model="location" @change="setLocation" placeholder="Type your location:"/>
+    <input type="autocomplete" v-model="location" @change="setLocation" placeholder="Type your location:"/>
+    <ul v-if="filteredLocations.length > 0">
+      <li v-for="l in filteredLocations" :key="l">{{ l }}</li>
+    </ul>
     <p>{{ selectedLocation }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 export default {
 
     setup(){
         const location = ref('');
         const selectedLocation = ref('');
+        const locations = ['Nevada', 'California', 'Alaska'];
 
         const setLocation = () => {
             selectedLocation.value = 'Location:' + location.value;
         };
+
+        const filteredLocations = computed(() => {
+          const loweredLocations = location.value.toLowerCase();
+          return locations.filter((l) => l.toLowerCase().includes(loweredLocations));
+
+        });
         return {
             location,
             selectedLocation,
-            setLocation
+            setLocation,
+            filteredLocations
         };
     },
 };
